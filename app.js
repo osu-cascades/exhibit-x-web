eae26cd3106ec64d0b4bdc62da13b4edcbbc4015
Max Mitchell <rat2043@gmail.com>
2022-01-17 14:22:30 -0800

    stub out form and route for posting sketches #5

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dashboardRouter = require('./routes/dashboard')
+var sketchRouter = require('./routes/sketch')
+
 const prisma = require('./prisma');
 
 var app = express();
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dashboard', dashboardRouter);
+app.use('/sketch', sketchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
