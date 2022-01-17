eaffa0c921dc5c751bcb715aa7b4666f8ad19a92
Max Mitchell <rat2043@gmail.com>
2022-01-17 15:15:06 -0800

    add dependencies for S3 and file uploads #5

--- routes/sketch.js
+++ routes/sketch.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var S3 = require('aws-sdk/clients/s3');
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  console.log(req.files);
   res.redirect('/');
 });
 
