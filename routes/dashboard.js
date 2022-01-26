4d129857a0dd394bd08097c75944eec22ba7a685
Colin Suckow <csuckow99@gmail.com>
2022-01-26 10:45:12 -0800

    Store hearbeat messages in db and show on dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,10 @@ const prisma = require('../prisma');
 /* GET dashboard page. */
 router.get('/', async function(req, res, next) {
     const sketches = await prisma.sketch.findMany();
-    res.render('dashboard', { sketches: sketches });
+    const { createdAt } = await prisma.sketch.findFirst({
+        orderBy: { createdAt: "desc"}
+      });
+    res.render('dashboard', { sketches: sketches, lastHeartbeat: createdAt });
 });
 
 module.exports = router;
