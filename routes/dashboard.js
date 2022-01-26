47744ed6ebc20ae84edde10e0b9f5d6c9490b1cd
Colin Suckow <csuckow99@gmail.com>
2022-01-26 13:33:23 -0800

    Add heartbeat string formatting and stale calculation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const prisma = require('../prisma');
+const moment = require('moment')
 
 /* GET dashboard page. */
 router.get('/', async function(req, res, next) {
@@ -8,8 +9,8 @@ router.get('/', async function(req, res, next) {
     const lastHeartbeat = await prisma.exhibitHeartbeat.findFirst({
         orderBy: { receivedAt: "desc"}
       });
-    console.log("lastHeartbeat %s", lastHeartbeat);
-    res.render('dashboard', { sketches: sketches, lastHeartbeat: lastHeartbeat != undefined ? lastHeartbeat.receivedAt : undefined });
+    const stale = lastHeartbeat ? moment.duration(moment().diff(lastHeartbeat.receivedAt)).asMinutes() > 5 : true;
+    res.render('dashboard', { sketches: sketches, lastHeartbeat: lastHeartbeat ? moment(lastHeartbeat.receivedAt).fromNow() : "Never", stale: stale });
 });
 
 module.exports = router;
