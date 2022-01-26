173e90dac721681ac35285bda3095ede62f25bd2
Colin Suckow <csuckow99@gmail.com>
2022-01-26 13:16:21 -0800

    Implement "Never" message if there are no heartbeats

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,10 +5,11 @@ const prisma = require('../prisma');
 /* GET dashboard page. */
 router.get('/', async function(req, res, next) {
     const sketches = await prisma.sketch.findMany();
-    const { createdAt } = await prisma.sketch.findFirst({
-        orderBy: { createdAt: "desc"}
+    const lastHeartbeat = await prisma.exhibitHeartbeat.findFirst({
+        orderBy: { receivedAt: "desc"}
       });
-    res.render('dashboard', { sketches: sketches, lastHeartbeat: createdAt });
+    console.log("lastHeartbeat %s", lastHeartbeat);
+    res.render('dashboard', { sketches: sketches, lastHeartbeat: lastHeartbeat != undefined ? lastHeartbeat.receivedAt : undefined });
 });
 
 module.exports = router;
