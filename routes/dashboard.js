08ce0d6eeeade27dbaaf23d4e89f3fcac2907807
Colin Suckow <csuckow99@gmail.com>
2022-01-26 16:25:24 -0800

    Allow use to select sketch to run

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,10 +8,23 @@ router.get('/', async function(req, res, next) {
     const sketches = await prisma.sketch.findMany();
     const lastHeartbeat = await prisma.exhibitHeartbeat.findFirst({
         orderBy: { receivedAt: "desc"}
-      });
+    });
+
+    const selectedSketch = await prisma.selectedSketch.findFirst({
+      orderBy: { createdAt: "desc"}
+    });
+    
     const stale = lastHeartbeat ? moment.duration(moment().diff(lastHeartbeat.receivedAt)).asMinutes() > 2 : true;
-    const activeSketch = lastHeartbeat && lastHeartbeat.activeSketch > 0 ? lastHeartbeat.activeSketch : undefined;
-    res.render('dashboard', { sketches: sketches, lastHeartbeat: lastHeartbeat ? moment(lastHeartbeat.receivedAt).fromNow() : "Never", stale: stale, activeSketch: activeSketch });
+    const activeSketchId = lastHeartbeat && lastHeartbeat.activeSketch > 0 ? lastHeartbeat.activeSketch : undefined;
+    const requestedSketchId = selectedSketch ? selectedSketch.sketchId : undefined;
+    res.render('dashboard', { 
+      sketches: sketches, 
+      lastHeartbeat: lastHeartbeat ? moment(lastHeartbeat.receivedAt).fromNow() : "Never", 
+      stale: stale,
+      activeSketch: activeSketchId,
+      activeRow: activeSketchId != undefined && activeSketchId == requestedSketchId ? activeSketchId : undefined,       //TODO: Clean up this stuff
+      pendingRow: requestedSketchId != undefined && activeSketchId != requestedSketchId ? requestedSketchId : undefined
+    });
 });
 
 module.exports = router;
