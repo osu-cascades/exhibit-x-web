c221abe1040da3339546fe035ad7f9d3dc144c10
Colin Suckow <csuckow99@gmail.com>
2022-02-02 14:32:42 -0800

    Add users card

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { signIn } = require('../utils/auth')
+const { signIn, checkIsAdmin } = require('../utils/auth')
 
 router.post('/signIn', async function(req, res, next) {
   const { credential } = req.body ;
   req.session.user = await signIn(credential);
   res.redirect('/');
 });
+
+router.post('/updateUser', checkIsAdmin, async function(req, res, next) {
+  await prisma.user.update({
+    where: {
+      email: req.body.user
+    },
+    data: {
+      admin: 
+    }
+  });
+})
 
 module.exports = router;
