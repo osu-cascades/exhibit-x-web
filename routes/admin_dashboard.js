eaed51a7ac37909b639caff7647aa066d37eb2ce
Max Mitchell <rat2043@gmail.com>
2022-02-18 15:50:45 -0800

    apply eslint to admin_dashboard.js #25

--- routes/admin_dashboard.js
+++ routes/admin_dashboard.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = new express.Router();
 const prisma = require('../prisma');
 const moment = require('moment');
-const { checkIsAdmin } = require('../utils/auth')
+const {checkIsAdmin} = require('../utils/auth');
 
 /* GET dashboard page. */
-router.get('/', checkIsAdmin,  async function(req, res, next) {
+router.get('/', checkIsAdmin, async function(req, res, next) {
   const sketches = await prisma.sketch.findMany();
   const lastHeartbeat = await prisma.exhibitHeartbeat.findFirst({
-    orderBy: { receivedAt: "desc"}
+    orderBy: {receivedAt: 'desc'},
   });
 
   const selectedDisplay = await prisma.selectedDisplay.findFirst({
-    orderBy: { createdAt: "desc"}
+    orderBy: {createdAt: 'desc'},
   });
 
   const users = await prisma.user.findMany();
@@ -21,29 +21,45 @@ router.get('/', checkIsAdmin,  async function(req, res, next) {
     include: {
       SketchesOnSchedules: {
         include: {
-          sketch: true
-        }
-      }
-    }
+          sketch: true,
+        },
+      },
+    },
   });
 
-  const stale = lastHeartbeat ? moment.duration(moment().diff(lastHeartbeat.receivedAt)).asMinutes() > 2 : true;
-  const activeDisplayId = lastHeartbeat && lastHeartbeat.activeDisplayId > 0 ? lastHeartbeat.activeDisplayId : undefined;
-  const requestedSketchId = selectedDisplay ? selectedDisplay.displayId : undefined;
+  const stale = lastHeartbeat ?
+    moment.duration(moment().diff(lastHeartbeat.receivedAt)).asMinutes() > 2 :
+    true;
+  const activeDisplayId = lastHeartbeat && lastHeartbeat.activeDisplayId > 0 ?
+    lastHeartbeat.activeDisplayId :
+    undefined;
+  const requestedSketchId = selectedDisplay ?
+    selectedDisplay.displayId :
+    undefined;
   const activeDisplayType = lastHeartbeat?.activeDisplayType;
   const requestedDisplayType = selectedDisplay?.type;
   res.render('admin_dashboard', {
     sketches: sketches,
-    lastHeartbeat: lastHeartbeat ? moment(lastHeartbeat.receivedAt).fromNow() : "Never",
+    lastHeartbeat: lastHeartbeat ?
+      moment(lastHeartbeat.receivedAt).fromNow() :
+      'Never',
     stale: stale,
     activeSketch: activeDisplayId,
     requestedDisplay: requestedSketchId,
-    activeRow: activeDisplayId != undefined && activeDisplayId == requestedSketchId && activeDisplayType == requestedDisplayType ? activeDisplayId : undefined,       //TODO: Clean up this stuff
-    pendingRow: requestedSketchId != undefined && activeDisplayId != requestedSketchId || activeDisplayType != requestedDisplayType ? requestedSketchId : undefined,
+    activeRow: activeDisplayId != undefined &&
+      activeDisplayId == requestedSketchId &&
+      activeDisplayType == requestedDisplayType ?
+        activeDisplayId :
+        undefined, // TODO: Clean up this stuff
+    pendingRow: requestedSketchId != undefined &&
+      activeDisplayId != requestedSketchId ||
+      activeDisplayType != requestedDisplayType ?
+        requestedSketchId :
+        undefined,
     users: users,
     schedules: schedules,
     runningType: activeDisplayType,
-    requestedType: requestedDisplayType
+    requestedType: requestedDisplayType,
   });
 });
 
