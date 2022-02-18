6e3dbec817fb076dc06be312e9a92b922aff0577
Max Mitchell <rat2043@gmail.com>
2022-02-18 15:46:42 -0800

    apply eslint to index.js #25

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
-const { getAuthLevel } = require('../utils/auth');
+const express = require('express');
+const router = new express.Router();
+const {getAuthLevel} = require('../utils/auth');
 
 /* GET home page. */
 router.get('/', getAuthLevel, async function(req, res, next) {
-  var success_message = "";
-  if(req.query.event == "upload_successful") {
-    success_message = "Your file was uploaded successfully";
+  let successMessage = '';
+  if (req.query.event == 'upload_successful') {
+    successMessage = 'Your file was uploaded successfully';
   }
-  res.render('index', { success_message });
+  res.render('index', {successMessage});
 });
 
 module.exports = router;
